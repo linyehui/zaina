bf0bc5971d93fa7e530dc5984de6d3dcba49b311
SND\debehe_cp <SND\debehe_cp@4ef1bfeb-0a34-4d02-9305-240e5b07a168>
2010-10-21 10:56:37 +0000

    修正网络连接bug

---
+++ coding/Zaina/Zaina/Service/M8Helper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.IO;
+using System.Diagnostics;
+using MeizuSDK.Core;
+using MeizuSDK.Presentation;
+
+namespace M8Helper
+{
+    class Network
+    {
+        public static void InitMeizuNewwork(HttpWebRequest request)
+        {
+            if (!Networking.GprsConnect(GprsAppType.Default))
+            {
+                Debug.WriteLine(Networking.Status.ToString());
+            }
+
+            //MessageBox.Show("NetworkingStatus 2: " + Networking.Status.ToString());
+            if (NetworkingStatus.EdgeProxy == Networking.Status)
+            {
+                //status = Networking.Status;
+                //MessageBox.Show("NetworkingStatus: " + status.ToString());
+
+                WebProxy wp = new WebProxy("10.0.0.172", 80);
+                request.Proxy = wp;
+            }
+        }
+    }
+}
